d451b2c9092b293f74a85791d1bd5674b5902900
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-01-05 10:54:59 +0100

    fix findIndex when ref is not defined

--- tree.js
+++ tree.js
@@ -476,11 +476,11 @@ function drawTree(svg, data, options) {
           dx = Math.max(Math.abs((x2-x1) / 2), 20),
           r1 = d.data.op ? 9 : 5;
 	
-      var inputSpread = 10;
+      var inputSpread = 8;
       var yOffset = 0;
       if (parent.children.length > 1) {
         var parentIndex = parent.children.findIndex(el => {
-          return el === d || el.data.ref == d.data.id;
+          return el === d || (el.data.ref && el.data.ref == d.data.id);
         });
         yOffset = inputSpread / (parent.children.length-1) * parentIndex - inputSpread/2;
       }
